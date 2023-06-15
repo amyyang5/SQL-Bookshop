667b92b0ae6df0fa98d0afb7c17f73f1f266078b
Amy <a.yang97@hotmail.com>
2023-06-15 12:33:56 +0100

    on q5

--- db/02-create-books-table.sql
+++ db/02-create-books-table.sql
@@ -1,3 +1,10 @@
 \c my_bookshop;
 
--- create the books table here
+CREATE TABLE books (
+    book_id SERIAL PRIMARY KEY,
+    title VARCHAR(50),
+    price_in_pence INT,
+    quantity_in_stock INT,
+    release_date DATE,
+    is_fiction BOOLEAN
+)
