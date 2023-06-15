fd6454b8d917c62ceb4d8b7af3f3092ec2ff4a2d
Amy <a.yang97@hotmail.com>
2023-06-15 16:14:46 +0100

    completed q10

--- db/07-create-authors-table.sql
+++ db/07-create-authors-table.sql
@@ -1,2 +1,8 @@
 \c my_bookshop
+ CREATE TABLE authors (
+    author_id SERIAL PRIMARY KEY,
+    author_name VARCHAR(50) NOT NULL,
+    fun_fact VARCHAR(300) NOT NULL
+);
 
+SELECT DISTINCT * FROM authors;
--- db/09-alter-books-table.sql
+++ db/09-alter-books-table.sql
@@ -1,2 +1,6 @@
 \c my_bookshop
+ALTER TABLE books
+ADD author_id INT, 
+ADD FOREIGN KEY (author_id) REFERENCES authors(author_id);
 
+SELECT * FROM books;
